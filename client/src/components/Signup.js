dd8219982c55468852e7bd978227c922069eb4a0
NiteshBisht0075 <Nitesh.bisht@intelligaia.com>
2021-06-19 22:28:46 +0530

    mern dev

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,20 +1,55 @@
 import React,{useState} from 'react'
+import { confirmPassword, validEmail,validName,validPassword} from '../regex/Regex';
 import { useHistory } from 'react-router-dom';
-const Signup =  () =>{
+
+const Signup = () => {
   const history =useHistory()
-  const [user,setUser] = useState({
+  const [user, setUser] = useState({
     name:"",email:"",password:"",cpassword:""
-  }); 
-  let name,value; 
+  });
+  const [nameErr, setNameErr] = useState(false);
+  const [emailErr, setEmailErr] = useState(false);
+  const [pwdError, setPwdError] = useState(false);
+  const [cpwdError, setCpwdError] = useState(false);
+
+  let name,value;
   const handleInput = (e) =>{
     name = e.target.name;
     value = e.target.value;
     setUser({...user,[name]:value});
   }
-  const PostData=async(e)=>{
-    e.preventDefault();
+  const validate = () => {
+    if(!validName.test(user.name)){
+      setNameErr(true);
+    }
+    else{
+      setNameErr(false);
+    }
+    if (!validEmail.test(user.email)) {
+       setEmailErr(true);
+    }
+    else{
+      setEmailErr(false);
+    }
+    if (!validPassword.test(user.password)) {
+       setPwdError(true);
+    }
+    else{
+      setPwdError(false)
+    }
+    if(confirmPassword.test(user.cpassword)){
+      setCpwdError(true)
+    }
+    else{
+      setCpwdError(false)
+    }
+ };
+
+  const PostData = async(e) =>{
+    e.preventDefault(); 
+    // user.name
     const {name,email,password,cpassword} = user;
-    const res = await fetch("/register",{
+        const res = await fetch("/register",{
          method: "POST",
          headers: {
            "Content-Type" :"application/json"
@@ -28,14 +63,13 @@ const Signup =  () =>{
       window.alert("invalid registration")
     }
     else{
-      window.alert("registration successful")
+      window.alert("Registration Success")
       history.push("/login")
     }
   }
   return (
     <div>
       <div className="signup-form">
-    
 		<div className="form-header">
 			<h2>Sign Up</h2>
 			<p>Fill out this form to start your free trial!</p>
@@ -47,6 +81,7 @@ const Signup =  () =>{
           value={user.name}
           onChange={handleInput}
           />
+       {nameErr && <p>Write valid name</p>}
     </div>
     <div className="form-group">
 			<label>Email Address</label>
@@ -54,6 +89,8 @@ const Signup =  () =>{
           value={user.email}
           onChange={handleInput}
           />
+      {emailErr && <p>Your email is invalid</p>}
+
     </div>
 		<div className="form-group">
 			<label>Password</label>
@@ -61,6 +98,7 @@ const Signup =  () =>{
             value={user.password}
             onChange={handleInput}
             />
+      {pwdError && <p>Password could be more secure</p>}
     </div>
 		<div className="form-group">
 			<label>Confirm Password</label>
@@ -68,12 +106,13 @@ const Signup =  () =>{
             value={user.cpassword}
             onChange={handleInput}    
             />
+       {cpwdError && <p>confirm password should be match</p>}     
     </div>        
 
 		<div className="form-group">
       <input type="submit" className="btn btn-primary btn-block btn-lg" name="signup" required="required"
       value="register"
-      onClick={PostData}
+      onClick={validate}
       />
 		</div>	
     </form>
